ecbfa4c30be95483fb7be837406e979c7fded837
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-04-11 08:27:10 -0400

    Create row and square elements through js

---
+++ etch-sketch.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const sketchContainer = document.getElementById("sketch-container");
+
+const row = document.createElement("div");
+row.classList.add("row");
+sketchContainer.appendChild(row);
+
+const square = document.createElement("div");
+square.classList.add("square");
